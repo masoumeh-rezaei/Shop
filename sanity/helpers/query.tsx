4782163eb8fad70533e95953b9e1ff32ad8bc882
Masomeh Rezaei <masomehrezaei83.r@gmail.com>
2025-07-30 12:35:19 +0330

    update query.tsx version 2.32

--- sanity/helpers/query.tsx
+++ sanity/helpers/query.tsx
@@ -2,14 +2,18 @@ import { defineQuery } from "next-sanity"
 import { sanityFetch } from "../lib/live";
 
 export const getProductBySlug=async(slug:string)=>{
-    const PRODUCT_BY_SLUG_QUERY=defineQuery(`*[_type == 'product' && slug.current == $slug] | order(name asc) [0]`);
+    const PRODUCT_BY_SLUG_QUERY=defineQuery(`*[_type == 'product' && slug.current == $slug][0] `);
     try{
+        console.log("Query sent to Sanity:", PRODUCT_BY_SLUG_QUERY);
+
+
         const product = await sanityFetch({
             query:PRODUCT_BY_SLUG_QUERY,
             params:{
                 slug,
             },
         });
+
         return product?.data || null
 
     }catch(err){
@@ -17,7 +21,8 @@ export const getProductBySlug=async(slug:string)=>{
     }
 }
 export const getAllCategories = async()=>{
-    const CATEGORIES_QUERY= defineQuery(`*[_type=='category'] | order(name asc)`);
+    const CATEGORIES_QUERY = defineQuery(`*[_type == 'category'] | order(title asc)`);
+
     try{
         const categories = await sanityFetch({
             query:CATEGORIES_QUERY,
