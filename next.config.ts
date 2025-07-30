1d7df6a6414db1bd896726bc40e52ab733a1833c
Masomeh Rezaei <masomehrezaei83.r@gmail.com>
2025-07-30 12:36:09 +0330

    update next.config.ts version 2.35

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images:{
     remotePatterns:[
-      {protocol:'https',hostname:'cdn.sanity.io'}
+      {protocol:'https',hostname:'cdn.sanity.io',pathname: "/images/**"}
     ]
   }
 };
